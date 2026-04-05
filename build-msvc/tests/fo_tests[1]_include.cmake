14b9c7df60a521d035eb90108e6be8cd72143ac7
Robert Pelloni <pelloni.robert@gmail.com>
2026-04-05 17:03:08 -0400

    chore: remove accidentally tracked local build and probe directories

--- build-msvc/tests/fo_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/hyper/workspace/bobfilez/build-msvc/tests/fo_tests[1]_tests.cmake")
-  include("C:/Users/hyper/workspace/bobfilez/build-msvc/tests/fo_tests[1]_tests.cmake")
-else()
-  add_test(fo_tests_NOT_BUILT fo_tests_NOT_BUILT)
-endif()
