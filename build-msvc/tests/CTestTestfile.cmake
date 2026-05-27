7561b34dfda852a63d0faeb3e93c2e7ea6032f1a
Robert Pelloni <pelloni.robert@gmail.com>
2026-05-27 11:03:13 -0400

    fix: recover lost OpenSSL CMake fix, Delete Dupes server.js, core/test cleanup from reflog (6 commits)

--- build-msvc/tests/CTestTestfile.cmake
+++ build-msvc/tests/CTestTestfile.cmake
@@ -6,4 +6,4 @@
 # testing this directory and lists subdirectories to be tested as well.
 include("C:/Users/hyper/workspace/bobfilez/build-msvc/tests/fo_tests[1]_include.cmake")
 add_test(fo_c_api_smoke "C:/Users/hyper/workspace/bobfilez/build-msvc/tests/fo_c_api_smoke.exe")
-set_tests_properties(fo_c_api_smoke PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/hyper/workspace/bobfilez/tests/CMakeLists.txt;50;add_test;C:/Users/hyper/workspace/bobfilez/tests/CMakeLists.txt;0;")
+set_tests_properties(fo_c_api_smoke PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/hyper/workspace/bobfilez/tests/CMakeLists.txt;52;add_test;C:/Users/hyper/workspace/bobfilez/tests/CMakeLists.txt;0;")
